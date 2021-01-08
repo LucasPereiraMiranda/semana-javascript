f0fd268886b919bd6e4ed5a6c1f5f1a50375ff93
Lucas Pereira Miranda <lucas.miranda1@aluno.ufop.edu.br>
2021-01-08 18:50:27 -0300

    Adicionar arquivos da explicação do professor

---
+++ src/explicacao/filter.js
@@ -0,0 +1,13 @@
+const users = [
+  { name: 'Ada Lovelace', premium: true },
+  { name: 'Grace Hopper', premium: false },
+  { name: 'Alan Turing', premium: true },
+  { name: 'Linus Torvalds', premium: false },
+  { name: 'Margaret Hamilton', premium: true }
+]
+
+const premiumUsers = users.filter((user) => {
+  return user.premium
+})
+
+console.log(users,premiumUsers)
